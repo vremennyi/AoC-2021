3eff07c31a7f3779dbcc4ee31a8ff76fae778fa5
Ural <vremennyi@rambler.ru>
2021-12-02 00:27:18 +0500

    Day 1: Sonar Sweep

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,52 @@
+import java.util.LinkedList
+import java.util.Queue
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var prev = Int.MAX_VALUE
+        var count = 0
+        for (s in input) {
+            s.toIntOrNull()?.let {
+                if (prev < it)
+                    count++
+                prev = it
+            }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val nums: Queue<Int> = LinkedList()
+        var prevSum = Int.MAX_VALUE
+        var count = 0
+        for (s in input) {
+            val currSum = nums.sum()
+            if (prevSum < currSum)
+                count++
+
+            s.toIntOrNull()?.let {
+                nums.add(it)
+            }
+
+            if (nums.size < 4)
+                continue
+
+            prevSum = currSum
+            nums.poll()
+        }
+        if (prevSum < nums.sum())
+            count++
+
+        return count
     }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("test_01")
 
-    val input = readInput("Day01")
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
+
+    val input = readInput("input_01")
     println(part1(input))
     println(part2(input))
 }
